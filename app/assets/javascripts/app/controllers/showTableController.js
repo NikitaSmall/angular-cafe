8d0630203f334ba017151ca152d17c5f8c2b1615
nikitasmall <nikitasosnov@yahoo.com>
2015-07-15 22:26:39 +0300

    minor fixes, new page for room-view implemented

---
+++ app/assets/javascripts/app/controllers/showTableController.js
@@ -0,0 +1,14 @@
+var showTableController = angular.module('showTableController', []);
+
+showTableController.controller('showTableCtrl', ['$scope', 'Table', 'Room', '$routeParams',
+    function($scope, Table, Room, $routeParams) {
+        $scope.room = Room.room({id: $routeParams.id});
+        $scope.points = Table.tables({id: $routeParams.id});
+
+        $scope.selectedPoint = null;
+
+        $scope.selectPoint = function (point) {
+            $scope.selectedPoint = point;
+        };
+
+    }]);
